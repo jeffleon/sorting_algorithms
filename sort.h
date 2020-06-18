4907e52401766af0b7289065b3b46b328c07b59c
matcls <manueltorresvesga@gmail.com>
2020-06-17 21:08:09 -0500

    added prototype for 7. Merge Sort 	void merge_sort(int *array, size_t size);

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void counting_sort(int *array, size_t size);
 
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 
 #endif
